c8a30db4244a809def692ff674814b9c8310696b
Thai Nguyen <nguyencaothai@gmail.com>
2020-09-17 08:20:49 -0700

    Setting up Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
-import { FlatList } from 'react-native';
+import { Text, ScrollView, FlatList} from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { PARTNERS } from '../shared/partners';
+
+function Mission(){
+    return (
+        <Card
+            title="Our Mission" 
+            wrapperStyle={{margin: 20}} >
+        <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other. </Text>
+        </Card>
+    );
+}
 class About extends Component {
     constructor(props) {
         super(props);
@@ -26,11 +35,7 @@ class About extends Component {
         };
         return (
             <ScrollView> 
-                <Card
-                    title="Our Mission" 
-                    wrapperStyle={{margin: 20}} >
-                    <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other. </Text>
-                </Card>
+                <Mission />
                 <Card
                     title="Community Partners" 
                     wrapperStyle={{margin: 20}} >
