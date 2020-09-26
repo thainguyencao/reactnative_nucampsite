68fff813d80b9a44b9d12e416a363d45eddb3527
Thai Nguyen <nguyencaothai@gmail.com>
2020-09-25 18:41:02 -0700

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,8 @@ import Directory from './DirectoryComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Favorites from './FavoritesComponent';
+
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator, createDrawerNavigator,
     DrawerItems } from 'react-navigation';
@@ -21,6 +23,28 @@ const mapDispatchToProps = {
     fetchPartners
 };
 
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 const ReservationNavigator = createStackNavigator(
     {
         Reservation: { screen: Reservation }
@@ -202,6 +226,20 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
+        Favorites: {
+            screen: FavoritesNavigator,
+            navigationOptions: {
+                drawerLabel: 'My Favorites',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='heart'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },        
         About: {            screen: AboutNavigator,
             navigationOptions: {
                 drawerLabel: 'About Us',
