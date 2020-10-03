3368456cf3ad683d03ea1b70e8997b7253469854
Thai Nguyen <nguyencaothai@gmail.com>
2020-10-03 07:55:01 -0700

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
     Modal, Button, StyleSheet,
-    Alert, PanResponder } from 'react-native';
+    Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 //import { CAMPSITES } from '../shared/campsites';
 //import { COMMENTS } from '../shared/comments';
@@ -88,7 +88,15 @@ function RenderCampsite(props) {
             return true;
         }
     });
-
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title,
+            message: `${title}: ${message} ${url}`,
+            url: url
+        },{
+            dialogTitle: 'Share ' + title
+        });
+    };
     if (campsite) {
         return (
             <Animatable.View
@@ -122,6 +130,15 @@ function RenderCampsite(props) {
                             reverse
                             onPress={() => props.onShowModal()}
                         />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            style={stylesheet.cardItem}
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
+                        />                        
                     </View> 
                 </Card>
             </Animatable.View>
